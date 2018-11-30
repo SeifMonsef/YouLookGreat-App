ba9d1d385a9320724a92021ec063e37a4f4213ea
Anas <anas_97@live.com>
2018-11-30 18:41:33 +0200

    index final imports

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Home from './views/Home/index';
 import store from './rootReducer';
+import Categories from './views/Categories/index';
+import Products from './views/ProductList';
+import Product from './views/Product/index';
+import Cart from './views/Cart/index';
 import Search from './views/Search';
 
 
@@ -16,7 +20,11 @@ ReactDOM.render(
       <App>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/categories" component={Categories} />
+          <Route path="/category/:categId" component={Products} />
+          <Route path="/product/:productId" component={Product} />
           <Route path="/search/:search" component={Search} />
+          <Route path="/cart" component={Cart} />
         </Switch>
       </App>
     </HashRouter>
