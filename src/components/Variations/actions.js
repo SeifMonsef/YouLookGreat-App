cbb7e1bf7a3e993ac49f4a340581c7d3cf06f8d4
SeifMonsef <seif.monsef@hotmail.com>
2018-11-30 17:26:39 +0200

    updated variationns actionss

--- src/components/Variations/actions.js
+++ src/components/Variations/actions.js
@@ -13,3 +13,13 @@ export const receiveVariations = variations => ({
   variations,
 });
 
+export const fetchVariations = productId => (dispatch) => {
+  dispatch(requestVariations());
+
+  return fetch(config.get('API_VARIATIONS_URL') + productId)
+    .then(response => response.json())
+    .then(json => dispatch(receiveVariations(json)))
+    .catch(() => {
+      dispatch(receiveVariations([]));
+    });
+};
